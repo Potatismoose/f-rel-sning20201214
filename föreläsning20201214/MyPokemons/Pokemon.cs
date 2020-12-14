945e47ad42996e237d81892672779b54512fea5c
Benny Christensen <benny@potatismoose.com>
2020-12-14 15:24:36 +0100

    Påbörjat konstruktorn för pokemons

--- föreläsning20201214/MyPokemons/Pokemon.cs
+++ föreläsning20201214/MyPokemons/Pokemon.cs
@@ -24,7 +24,94 @@ namespace föreläsning20201214.MyPokemon
 
         public Pokemon()
         {
+            Random rand = new Random();
+            int randomNr = rand.Next(0, 4);
+            string[] name = new string[4] { "Rypherior",  "Charizard",  "Weedle",  "Squirtle"  };
+            string[] type = new string[4] { "Ground", "Fire", "Grass", "Water" };
+            switch (randomNr)
+            {
+                case 0: //Rypherior
+                    string[] chargedAttackRy = new string[2] { "Earthquake", "Rock Wrecker" };
+                    
+                    
+                    this.name = name[randomNr];
+                    this.type = type[randomNr];
+                    this.ChargedAttack = chargedAttackRy[rand.Next(0, 2)];
+                    switch (chargedAttackRy[randomNr])
+                    {
+                        case "Earthquake":
+                            this.ChargedAttackDamage = 140;
+                            break;
+                        case "Rock Wrecker":
+                            this.ChargedAttackDamage = 110;
+                            break;
+                    }
+                    this.Hp = rand.Next(119, 217);
+                    this.AttackStrength = rand.Next(3, 16);
+                    this.AttackChargedTo = 0;
 
+                    break;
+                case 1: //Charizard
+                    string[] chargedAttackCh = new string[2] { "Earthquake", "Rock Wrecker" };
+
+
+                    this.name = name[randomNr];
+                    this.type = type[randomNr];
+                    this.ChargedAttack = chargedAttackCh[rand.Next(0, 2)];
+                    switch (chargedAttackCh[randomNr])
+                    {
+                        case "Earthquake":
+                            this.ChargedAttackDamage = 140;
+                            break;
+                        case "Rock Wrecker":
+                            this.ChargedAttackDamage = 110;
+                            break;
+                    }
+                    this.Hp = rand.Next(119, 217);
+                    this.AttackStrength = rand.Next(3, 16);
+                    this.AttackChargedTo = 0;
+                    break;
+                case 2: //Weedle
+                    string[] chargedAttackWe = new string[2] { "Struggle", "Return" };
+
+
+                    this.name = name[randomNr];
+                    this.type = type[randomNr];
+                    this.ChargedAttack = chargedAttackWe[rand.Next(0, 2)];
+                    switch (chargedAttackWe[randomNr])
+                    {
+                        case "Earthquake":
+                            this.ChargedAttackDamage = 140;
+                            break;
+                        case "Rock Wrecker":
+                            this.ChargedAttackDamage = 110;
+                            break;
+                    }
+                    this.Hp = rand.Next(119, 217);
+                    this.AttackStrength = rand.Next(3, 16);
+                    this.AttackChargedTo = 0;
+                    break;
+                case 3: //Squirtle
+                    string[] chargedAttackSq = new string[2] { "Water Pulse", "Aqua Jet" };
+
+
+                    this.name = name[randomNr];
+                    this.type = type[randomNr];
+                    this.ChargedAttack = chargedAttackSq[rand.Next(0, 2)];
+                    switch (chargedAttackSq[randomNr])
+                    {
+                        case "Earthquake":
+                            this.ChargedAttackDamage = 140;
+                            break;
+                        case "Rock Wrecker":
+                            this.ChargedAttackDamage = 110;
+                            break;
+                    }
+                    this.Hp = rand.Next(119, 217);
+                    this.AttackStrength = rand.Next(3, 16);
+                    this.AttackChargedTo = 0;
+                    break;
+            }
         }
     }
 }
